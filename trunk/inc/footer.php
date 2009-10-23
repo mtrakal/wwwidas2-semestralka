860486ce36c27b6e857851b9cd8cea80ff4d9f3a
mtrakal <mtrakal@336c5a50-bf6b-11de-a250-c7f789e21d83>
2009-10-23 00:45:46 +0000

    Pokus prace s SVN :)

---
+++ trunk/inc/footer.php
@@ -0,0 +1,29 @@
+                <!-- end #sidebar -->
+                <div class="cleaner both">&nbsp;</div>
+            </div>
+            <!-- end #page -->
+        </div>
+        <div id="footer-content">
+            <div class="column1">
+                <h2>Semestrální práce z IWWW a IDAS2</h2>
+                <p><strong>O co se jedná:</strong> Tento web začal vznikat, jako semestrální práce na IWWW a IDAS2. Základem byl layout stažený z freetemplates.org a upravený dle potřeb <a href="/">filmotéky</a>. Jako databázový layer je využito <a href="http://dibiphp.com" title="Database Abstraction Library">dibi</a> a zbytek? Zbytek splácal <a href="http://blog.trtkal.net">trtkal</a> tak nějak po svém :)</p>
+            </div>
+            <div class="column2">
+                <ul class="list">
+                    <li><a href="http://blog.trtkal.net">Trtkalovo hlavní pískoviště, kde si plácá to svoje</a></li>
+                    <li><a href="http://www.drevoteam.cz">Dřevoteam - truhlářství - mistrem svého oboru</a></li>
+                </ul>
+            </div>
+        </div>
+        <div id="footer">
+            <p>(c) 2009 <a href="http://blog.trtkal.net">Trtkal</a> pro semestrální práci z předmětu IWWW a IDAS2.<br />
+	CSS template využit a upraven z <a href="http://www.freecsstemplates.org">freetemplates.org.</a></p>
+        </div>
+        <!-- end #footer -->
+        <?php
+        //include_once "/classes/lang.php";
+        //language();
+        //echo lang_show_flags($lang_array_sort, $lang_ma);
+        ?>
+    </body>
+</html>
---
+++ trunk/index.php
@@ -0,0 +1,66 @@
+<?php
+include_once "./inc/header.php";
+?>
+                    <li class="current_page_item"><a href="/index_.php">Hlavní stránka</a></li>
+                    <li><a href="/index_filmoteka.php">Seznam filmů</a></li>
+                    <li><a href="#">Administrace</a></li>
+                </ul>
+            </div>
+            <!-- end #menu -->
+            <div id="page">
+                <div id="content">
+                        <!--<div id="banner"><img src="/style/img/img07.jpg" alt="" /></div>-->
+                    <div class="post">
+                        <h2 class="title"><a href="#">Interní filmová databáze</a></h2>
+                        <!--<p class="meta">Fabuleux destin d'Amélie Poulain, Le</p>-->
+                        <div class="entry">
+                            <ul>
+                                <li><strong>Popis</strong>
+                                    <ul>
+                                        <li>Správa osobní filmotéky s malou půjčovnou</li>
+                                        <li>Hodnocení filmů pouze správcem</li>
+                                        <li>Možnost stahování titulků</li>
+                                        <li>Zobrazení žánru, délky, ... filmu</li>
+                                    </ul>
+                                </li>
+                                <li><strong>Uživatelské role</strong>
+                                    <ul>
+                                        <li>Viz obrázek z Oracle DB</li>
+                                    </ul>
+                                </li>
+                                <li><strong>Předpokládaný rozsah</strong>
+                                    <ul>
+                                        <li>Databáze: <a href="/img/oracle.png">obrázek (28 kB)</a></li>
+                                        <li>WWW a CSS v rozsahu do 10 stránek s generovaným obsahem</li>
+                                    </ul>
+                                </li>
+
+                            </ul>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+                            <p class="links"><a href="#" class="edit">Upravit</a></p>
+                        </div>
+                    </div>
+                    <div class="cleaner both">&nbsp;</div>
+                </div>
+                <!-- end #content -->
+                <div id="sidebar">
+                    <ul>
+                        <li>
+                            <h2>Statistiky databáze</h2>
+                            <ul>
+                                <li><div>Celkem filmů</div>99 241</li>
+                                <li><div>Registrovaných uživatelů</div>15</li>
+                                <li><div>Nejpůjčovanější filmy</div>
+                                    <ol>
+                                        <li>Amélie z Montmartru</li>
+                                        <li>Cesta do fantazie</li>
+                                    </ol>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+<?php
+include_once "./inc/footer.php";
+?>
