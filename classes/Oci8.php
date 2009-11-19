a0b8e47a44fdf64f23945bb7aa9ecf721d7e2470
mtrakal <mtrakal@336c5a50-bf6b-11de-a250-c7f789e21d83>
2009-11-19 01:19:47 +0000

    DB - načítání dat do filmů Autocomplete fičura + načítání z oraclu Autocomplete - propojení na filmy

--- classes/Oci8.php
+++ classes/Oci8.php
@@ -16,7 +16,7 @@ class Oci8 {
                 'database' => 'sql101.upceucebny.cz:1521/oracle10',
                 'username' => 'st22312',
                 'password' => 'oracletrtkal',
-                'charset' => 'utf-8'
+                'charset' => 'UTF8'
             ));
         } catch (DibiException $e) {
             echo get_class($e), ': ', $e->getMessage(), "\n";
@@ -65,6 +65,25 @@ class Oci8 {
         if(dibi::isConnected()) {
             $result= dibi::query("select role_id as ID, role as ROLE from trole");
             $row = $result->fetchPairs();
+            return $row;
+        }
+    }
+
+    public function Autocomplete($var) {
+        $result = null;
+        $row = array();
+        if(dibi::isConnected()) {
+            $result= dibi::query("select film_id as IDFILMU, cz as CZ, en as EN, original as ORIGINAL, to_char(substr(popis,0,50)) as POPIS from ttitul where lower(ttitul.cz) like lower('%".$var."%') or lower(ttitul.en) like lower('%".$var."%') or lower(ttitul.original) like lower('%".$var."%')");
+            $row = $result->fetchAll();
+            return $row;
+        }
+    }
+    public function MovieGet($id) {
+        $result = null;
+        $row = array();
+        if(dibi::isConnected()) {
+            $result= dibi::query("select film_id as IDFILMU, cz as CZ, en as EN, original as ORIGINAL, csfd as CSFD, imdb as IMDB, rok_vydani as ROK, delka as DELKA, popis as POPIS from ttitul where film_id=".$id);
+            $row = $result->fetchAll();
             return $row;
         }
     }
---
+++ pokus/autocomplete.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * AutoComplete Field - PHP Remote Script
+ *
+ * This is a sample source code provided by fromvega.
+ * Search for the complete article at http://www.fromvega.com
+ *
+ * Enjoy!
+ *
+ * @author fromvega
+ *
+ */
+
+// check the parameter
+if(isset($_GET['part']) and $_GET['part'] != '') {
+// initialize the results array
+    $results = array();
+
+    // search colors
+    require_once dirname(__FILE__) . '/../classes/Oci8.php';
+    $db = new Oci8();
+    $pom = $db->Autocomplete($_GET['part']);
+    unset ($db);
+    foreach ($pom as $variable) {
+    // if it starts with 'part' add to results
+    //if( strpos($variable, 'amel') === 0 ) {
+        $results[] = $variable['ORIGINAL']."<div class=\"font-small\"><p>CZ: ".$variable['CZ']."<br />EN:".$variable['EN']."<br />".$variable['POPIS']."&hellip;</p></div>";
+    //}
+    };
+    unset ($pom);
+
+    // return the array as json with PHP 5.2
+    echo json_encode($results);
+
+// or return using Zend_Json class
+//require_once('Zend/Json/Encoder.php');
+//echo Zend_Json_Encoder::encode($results);
+}
